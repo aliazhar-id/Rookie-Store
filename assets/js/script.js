dec46f07a449af9dff3d7e3bf2548ab096c7b20e
aliazhar-id <aliazhar0555@gmail.com>
2021-04-03 19:02:29 +0700

    Update 03/04/21

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+function notBuy() {
+	alert("Websitenya belom jadi, Jika ingin membeli anda bisa menghubungi kontak yang tercantum di bagian paling bawah halaman ini");
+}
+
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+  	alert("What are you doing? trying to steal my code? Fuck You!")
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
