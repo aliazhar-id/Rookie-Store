60808cdf9ea8cfd243ff4d958b187e5ca8d2cc1e
aliazhar-id <aliazhar0555@gmail.com>
2021-04-04 20:42:18 +0700

    Add and remake some layout with javascript and and much more!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,19 @@
 function notBuy() {
 	alert("Websitenya belom jadi, Jika ingin membeli anda bisa menghubungi kontak yang tercantum di bagian paling bawah halaman ini");
 }
-
-document.onkeydown = function(e) {
-  if(event.keyCode == 123) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-  	alert("What are you doing? trying to steal my code? Fuck You!")
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-     return false;
-  }
+function login(st) {
+	if (st == 1) {
+		document.getElementById("body").style.overflow = "hidden";
+		document.getElementById("login").style.display = "flex";
+		window.scrollTo(0,0);
+	}
+	else {
+		document.getElementById("login").style.display = "none";
+		document.getElementById("body").style.overflow = "auto";
+	}
+}
+document.onkeydown = function(e) { 
+  if((event.keyCode == 123) || (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) || (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) || (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) || (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0))) {
+		return false;
+	}
 }
