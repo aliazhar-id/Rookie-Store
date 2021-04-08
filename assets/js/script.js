842fee0d27910cbd5746c97d8092af0b28cc9d53
aliazhar-id <aliazhar0555@gmail.com>
2021-04-08 21:53:10 +0700

    Update 08/04/2021

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,29 @@
+var docHeight = document.body.scrollHeight;
+function scSmooth(to, duration) {
+    if (duration < 0) return;
+    var scrollTop = document.body.scrollTop + document.documentElement.scrollTop;
+    var difference = to - scrollTop;
+    var perTick = difference / duration * 10;
+
+    setTimeout(function() {
+      scrollTop = scrollTop + perTick;
+      document.body.scrollTop = scrollTop;
+      document.documentElement.scrollTop = scrollTop;
+      if (scrollTop === to) return;
+      scSmooth(to, duration - 10);
+    }, 10);
+  }
 function notBuy() {
-	alert("Websitenya belom jadi, Jika ingin membeli anda bisa menghubungi kontak yang tercantum di bagian paling bawah halaman ini");
+	 question = confirm("\bRookieStore\b Saat ini masih dalam tahap alpha, \nJika anda ingin order/membeli bisa langsung menghubungi kontak yang tercantum di bagian paling bawah halaman ini. ");
+	if (question == true) {
+	  window.scSmooth(docHeight, 500);
+	} 
 }
 function login(st) {
 	if (st == 1) {
-		document.getElementById("body").style.overflow = "hidden";
+		document.body.style.overflow = "hidden";
 		document.getElementById("login").style.display = "flex";
-		window.scrollTo(0,0);
+		window.scrollTo({ top: 0, behavior: 'smooth' });
 	}
 	else {
 		document.getElementById("login").style.display = "none";
@@ -16,4 +34,7 @@ document.onkeydown = function(e) {
   if((event.keyCode == 123) || (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) || (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) || (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) || (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0))) {
 		return false;
 	}
+}
+function scDown() {
+	window.scSmooth(docHeight, 500);
 }
