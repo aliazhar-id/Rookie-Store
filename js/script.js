75635bdf46465ce113133c9c2cea4fa438ef2909
aliazhar-id <aliazhar0555@gmail.com>
2021-08-04 09:43:19 +0700

    Add Modal, make code more readable, disable some shortcuts, remove unused code, etc;

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-// document.onkeydown = (e) => {
-// 	if ((e.keyCode == 123) || (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) || (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) || (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) || (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0))) {
-// 		return false;
-// 	}
-// }
+document.onkeydown = (e) => {
+	if ((e.keyCode == 123) || (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) || (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) || (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) || (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0))) {
+		return false;
+	}
+}
 
 //when the browser is refreshed scroll the window to the top of the page
 window.onbeforeunload = () => {
@@ -39,12 +39,74 @@ function hideForm() {
 }
 
 const form = document.querySelector('form'),
-overlay = document.querySelector('.overlay'),
-btnCloseForm = document.querySelector('.btncloseform'),
-signinButton = document.querySelector('.header-signin-button'),
-hamburger = document.querySelector('.hamburger');
+	overlay = document.querySelector('.overlay'),
+	btnCloseForm = document.querySelector('.btncloseform'),
+	signinButton = document.querySelector('.header-signin-button'),
+	hamburger = document.querySelector('.hamburger');
 
 signinButton.onclick = showForm;
 hamburger.onclick = showForm;
 overlay.onclick = hideForm;
 btnCloseForm.onclick = hideForm;
+
+// hide marquee on onclick
+const marquee = document.querySelector('marquee');
+marquee.addEventListener('click', function () {
+	this.style.display = 'none';
+	scrollToDown();
+});
+
+// scroll to bottom when headerLogo got clicked
+const headerLogo = document.querySelector('.header-logo');
+headerLogo.onclick = scrollToDown;
+
+// scrollToDown
+function scrollToDown() {
+	let t = document.body.scrollHeight,
+		d = 500;
+	if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) return;
+	if (d < 0) return;
+	var scrollTop = document.body.scrollTop + document.documentElement.scrollTop;
+	var difference = t - scrollTop;
+	var perTick = difference / d * 10;
+
+	setTimeout(() => {
+		scrollTop += perTick;
+		document.body.scrollTop = scrollTop;
+		document.documentElement.scrollTop = scrollTop;
+		if (scrollTop === t) return;
+		scrollToDown(t, d - 10);
+	}, 10);
+}
+
+// Modal
+var modal = document.querySelector('.modal');
+const modalCloseBtn = document.getElementById('modal-close-btn');
+const modalOkBtn = document.getElementById('modal-ok-btn');
+
+const card = document.querySelectorAll('.card');
+card.forEach((x) => {
+	x.addEventListener('click', () => {
+		modal.style.display = "block";
+		document.body.style.overflow = 'hidden';
+	});
+});
+
+// function to hide modal
+function hideModal() {
+	modal.style.display = "none";
+	document.body.style.overflow = 'auto';
+}
+
+modalCloseBtn.onclick = hideModal;
+
+modalOkBtn.onclick = () => {
+	hideModal();
+	scrollToDown();
+}
+
+window.onclick = event => {
+	if (event.target == modal) {
+		hideModal();
+	}
+}
