93bc0a0f9088f10bf98b4e755e020e0f852d63a7
aliazhar-id <aliazhar0555@gmail.com>
2021-08-08 19:27:50 +0700

    Fix animation form using css animation, remove code for blocking some keyboard actions, fix scroll stuck on small device, disable right click event;

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
-//Disable some keyboard shortcut actions.
-document.onkeydown = (e) => {
-	if ((e.keyCode == 123) || (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) || (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) || (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) || (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0))) {
-		return;
-	}
-}
-
 // When the browser is refreshed scroll the window to the top of the page
 window.onbeforeunload = () => {
 	window.scrollTo(0, 0);
 }
+
+// Disable right clicking event
+document.body.addEventListener('contextmenu', (e) => e.preventDefault());
 
 const form = document.querySelector('form'),
 	overlay = document.querySelector('.overlay');
@@ -17,32 +13,18 @@ const form = document.querySelector('form'),
 function showForm() {
 	document.body.style.overflow = 'hidden';
 	form.style.display = 'block';
-	setTimeout(() => {
-		form.style.opacity = '0.3';
-	}, 80);
-
-	setTimeout(() => {
-		form.style.opacity = '0.5';
-	}, 150);
-
-	setTimeout(() => {
-		form.style.opacity = '1';
-	}, 300);
 	overlay.style.display = 'flex';
+	form.style.animation = 'showFormOpacity .35s forwards linear';
 }
 
 // Function to hide form element
 function hideForm() {
 	document.body.style.overflow = 'auto';
-	form.style.opacity = '0.5';
-	setTimeout(() => {
-		form.style.opacity = '0.3';
-	}, 150);
-
+	form.style.animation = 'hideFormOpacity .5s forwards linear';
 	setTimeout(() => {
 		form.style.display = 'none';
 		overlay.style.display = 'none';
-	}, 300);
+	}, 550);
 }
 
 // To show and hide form put the class, id, or tag name into array below, which mean index 0 to show Form; and index 1 to hide form, when got clicked;
@@ -52,7 +34,6 @@ formEvents.forEach((x, i) => {
 		document.querySelector(x).onclick = (i === 0) ? showForm : hideForm;
 	});
 });
-
 
 // Hide marquee on onclick
 const marquee = document.querySelector('marquee');
@@ -67,21 +48,7 @@ headerLogo.onclick = scrollToDown;
 
 // scrollToDown
 function scrollToDown() {
-	const t = document.body.scrollHeight,
-		d = 500;
-	if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) return;
-	if (d < 0) return;
-	var scrollTop = document.body.scrollTop + document.documentElement.scrollTop;
-	var difference = t - scrollTop;
-	var perTick = difference / d * 10;
-
-	setTimeout(() => {
-		scrollTop += perTick;
-		document.body.scrollTop = scrollTop;
-		document.documentElement.scrollTop = scrollTop;
-		if (scrollTop === t) return;
-		scrollToDown(t, d - 10);
-	}, 10);
+	window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
 }
 
 // Modal
